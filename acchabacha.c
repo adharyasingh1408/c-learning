fe1a673eae0102cb380016a1c257ddb8af1cfc4c
Adharya Singh <adharyasingh1408@gmail.com>
2026-08-30 19:51:49 +0530

    add project 1

---
+++ acchabacha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+float a,b,c,d,e ;
+float sum,percent ;
+printf("enter the marks of five subjects\n");
+scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+sum=a+b+c+d+e;
+percent=(sum/500)*100;
+printf("sum=%f\n",sum); 
+printf("percentage=%f\n",percent); 
+
+
+return 0;
+
+
+
+
+
+}
+
+
+
